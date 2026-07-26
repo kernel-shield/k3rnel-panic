6a77a724de642850686464266b72867ccd330bee
k3rnel pan1c <k3rnelshield@gmail.com>
2026-07-26 14:35:14 -0500

    Fix db import path in auth.routes.js

--- auth.routes.js
+++ auth.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const db = require('../db');
+const db = require('./db');
 const { hashPassword, comparePassword, setUserCookie, clearUserCookie, requireAuth } = require('../auth');
 
 const router = express.Router();
