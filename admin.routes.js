05a8a927eb215c30f0924ac4cb49f1b1cf9c5489
k3rnel pan1c <k3rnelshield@gmail.com>
2026-07-26 14:42:41 -0500

    Update admin.routes.js

--- admin.routes.js
+++ admin.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const db = require('./db');
-const { setAdminCookie, clearAdminCookie, requireAdmin } = require('../auth');
+const { setAdminCookie, clearAdminCookie, requireAdmin } = require('./auth');
 
 const router = express.Router();
 
